998a6fc05b18813a4982940808dde9c6ca80a9bf
Ophraj <95223163+Ophraj@users.noreply.github.com>
2024-03-11 18:03:09 +0200

    Create song.h

---
+++ song.h
@@ -0,0 +1,14 @@
+#ifndef SONG_H_
+#define SONG_H_
+
+int count_note = 24;
+String notes[] = {"DO1","DO1","RE","DO1","FA","MI","DO1","DO1","RE","DO1","SOL","FA","DO1","DO1","DO2","LA","FA","MI","RE","LA","LA","FA","SOL","FA"};
+int duration_note[] = {500,500,500,500,500,1000,500,500,500,500,500,1000,500,500,500,500,500,500,1000,500,500,500,500,1000};
+int num_notes[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+int count_note_welcome = 15;
+String notes_welcome[] = {"MI","MI","FA","SOL","SOL","FA","MI","RE","DO1","DO1","RE","MI","MI","RE","RE"};
+int duration_note_welcome[] = {500,500,500,500,500,500,500,500,500,500,500,500,500,500,1000};
+int num_notes_welcome[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+#endif
